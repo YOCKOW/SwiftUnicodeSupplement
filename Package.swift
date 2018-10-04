327bf312dfeb2002aa2069f1604517de51a6d710
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-04 17:33:06 +0900

    Fix dependencies.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(name: "IDNA", dependencies:[]),
     .target(name: "UCD", dependencies:[]),
-    .target(name: "UnicodeSupplement", dependencies: []),
+    .target(name: "UnicodeSupplement", dependencies: ["IDNA", "UCD"]),
     .testTarget(name: "UnicodeSupplementTests", dependencies: ["UnicodeSupplement"]),
   ]
 )
