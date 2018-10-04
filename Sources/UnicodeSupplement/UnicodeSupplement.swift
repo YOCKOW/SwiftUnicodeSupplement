9c2089f04e372066c0b08fe7364f5ea28e4d7446
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-04 15:20:59 +0900

    Export a function defined in "INDA".

--- Sources/UnicodeSupplement/UnicodeSupplement.swift
+++ Sources/UnicodeSupplement/UnicodeSupplement.swift
@@ -4,4 +4,8 @@
      Licensed under MIT License.
      See "LICENSE.txt" for more information.
  ************************************************************************************************ */
- 
+
+// Import "IDNA" module,
+// that means export `func idnaStatus(usingSTD3ASCIIRules std3:Bool = true,
+// idna2008Compatible idna2008:Bool = false) -> Unicode.IDNAStatus?` of `Unicode.Scalar`.
+import IDNA
