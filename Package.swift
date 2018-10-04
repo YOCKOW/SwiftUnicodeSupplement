1d393c6031c04eb0a4cc41a1b7ad3227de09c56e
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-04 14:04:00 +0900

    Change structure of the package.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,9 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(name: "UnicodeSupplement", dependencies: []),
+    .target(name: "IDNA", dependencies:[]),
+    .target(name: "UCD", dependencies:[]),
+    .target(name: "UnicodeSupplement", dependencies: ["IDNA", "UCD"]),
     .testTarget(name: "UnicodeSupplementTests", dependencies: ["UnicodeSupplement"]),
   ]
 )
