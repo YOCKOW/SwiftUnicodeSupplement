6cbb46ad34566a82844a0da124c99d5d1a4419ea
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-08 13:09:05 +0900

    Implement _UnicodePredicate that represents ranges of Unicode.Scalar.

--- Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
+++ Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
@@ -2,13 +2,26 @@ import XCTest
 @testable import UnicodeSupplement
 
 final class UnicodeSupplementTests: XCTestCase {
-  func test() {
-    // How to test...
+  func test_UnicodePredicate() {
+    let array: [UInt32] = [0x20819, 0x30819] // "A"..."Z", "a"..."z"
+    let predicate = _UnicodePredicate(array)
+    XCTAssertFalse(predicate.contains("0"))
+    XCTAssertFalse(predicate.contains("@"))
+    XCTAssertTrue(predicate.contains("A"))
+    XCTAssertTrue(predicate.contains("M"))
+    XCTAssertTrue(predicate.contains("Z"))
+    XCTAssertFalse(predicate.contains("["))
+    XCTAssertFalse(predicate.contains("`"))
+    XCTAssertTrue(predicate.contains("a"))
+    XCTAssertTrue(predicate.contains("m"))
+    XCTAssertTrue(predicate.contains("z"))
+    XCTAssertFalse(predicate.contains("{"))
+    XCTAssertFalse(predicate.contains("あ"))
   }
   
   
   static var allTests: [(String, (UnicodeSupplementTests) -> () -> ())] = [
-    ("test", test),
+    ("test_UnicodePredicate", test_UnicodePredicate),
   ]
 }
 
