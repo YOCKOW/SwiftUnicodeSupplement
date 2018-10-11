cdf3fde07035c66bac19153335ed8c038d971315
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-11 13:02:11 +0900

    Change layout of `_UnicodeAssociativeArray<T>`
    It may improve the performance.

--- Sources/UnicodeSupplement/UnicodeAssociativeArray.swift
+++ Sources/UnicodeSupplement/UnicodeAssociativeArray.swift
@@ -5,70 +5,48 @@
      See "LICENSE.txt" for more information.
  ************************************************************************************************ */
 
-private func _uint32_contains(_ integer:UInt32, _ value:UInt32) -> (Bool, canBreak:Bool) {
-  let start = integer >> 11
-  let length = integer & 0b11111111111
-  if value < start { return (false, true) }
-  if value <= start + length && value >= start { return (true, false) }
-  return (false, false)
-}
-
 internal struct _UnicodeAssociativeArray<T> {
-  fileprivate enum _Array {
-    /// An array of (`UInt32`, `T`).
-    ///
-    /// Each integer indicates a range of Unicode scalar.
-    /// * Upper 21 bits: The start value of scalar.
-    /// * Lower 11 bits: The length of the range.
-    ///
-    /// The instance of `T` is associated with the Unicode scalars contained in the range.
-    case table([(UInt32, T)])
-    
-    // when `T` is `Void`...
-    /// Only ranges.
-    case predicate([UInt32])
+  /// An array of (`UInt32`, `T`).
+  ///
+  /// Each integer indicates a range of Unicode scalar.
+  /// * Upper 21 bits: The start value of scalar.
+  /// * Lower 11 bits: The length of the range.
+  ///
+  /// The instance of `T` is associated with the Unicode scalars contained in the range.
+  /// When `T` is `Void`, it represents just ranges of Unicode scalars.
+  private let _array: [(UInt32, T)]
+  
+  internal init(_ table:[(UInt32, T)], alreadySorted:Bool = false) {
+    self._array = alreadySorted ? table : table.sorted { $0.0 < $1.0 }
   }
   
-  private let _array: _Array
-  
-  internal init(_ table:[(UInt32, T)], sort: Bool = true) {
-    self._array = .table(sort ? table.sorted { $0.0 < $1.0 } : table)
-  }
-  
-  internal init(_ table:[(UInt32, Void)], sort: Bool = true) {
-    let ranges:[UInt32] = table.map { $0.0 }
-    self._array = .predicate(sort ? ranges.sorted() : ranges)
-  }
-  
-  internal func associatedValue(for scalar:Unicode.Scalar) -> T? {
-    guard case .table(let table) = self._array else { preconditionFailure() }
-    
+  internal func value(for scalar:Unicode.Scalar) -> T? {
     let value = scalar.value
-    for pair in table {
-      let result = _uint32_contains(pair.0, value)
-      if result.0 { return pair.1 }
-      if result.canBreak { return nil }
+    for pair in self._array {
+      let start = pair.0 >> 11
+      if value < start { return nil } // because _array must be sorted.
+      
+      let length = pair.0 & 0b11111111111
+      if value <= start + length && value >= start { return pair.1 }
     }
     return nil
+  }
+  
+  internal subscript(_ scalar:Unicode.Scalar) -> T? {
+    return self.value(for:scalar)
   }
 }
 
 internal typealias _UnicodePredicate = _UnicodeAssociativeArray<Void>
 
 extension _UnicodeAssociativeArray where T == Void {
-  internal init(_ ranges:[UInt32], sort: Bool = true) {
-    self._array = .predicate(sort ? ranges.sorted() : ranges)
+  internal init(_ ranges:[UInt32], alreadySorted:Bool = false) {
+    assert(MemoryLayout<UInt32>.size == MemoryLayout<(UInt32, Void)>.size,
+           "This method expects that the sizes of UInt32 and (UInt32, Void) are equal.")
+    self.init(unsafeBitCast(ranges, to:Array<(UInt32, Void)>.self), alreadySorted:alreadySorted)
   }
   
   internal func contains(_ scalar:Unicode.Scalar) -> Bool {
-    guard case .predicate(let ranges) = self._array else { preconditionFailure() }
-    
-    let value = scalar.value
-    for range in ranges {
-      let result = _uint32_contains(range, value)
-      if result.0 { return true }
-      if result.canBreak { return false }
-    }
-    return false
+    return self.value(for:scalar) != nil
   }
 }
--- Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
+++ Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
@@ -4,36 +4,43 @@ import XCTest
 final class UnicodeSupplementTests: XCTestCase {
   func test_UnicodeAssociativeArray() {
     let array: [(UInt32, String)] = [(0x20819, "Upper"), (0x30819, "Lower")] // "A"..."Z", "a"..."z"
-    let data = _UnicodeAssociativeArray<String>(array, sort: false)
-    XCTAssertNil(data.associatedValue(for:"0"))
-    XCTAssertNil(data.associatedValue(for:"@"))
-    XCTAssertEqual(data.associatedValue(for:"A"), "Upper")
-    XCTAssertEqual(data.associatedValue(for:"M"), "Upper")
-    XCTAssertEqual(data.associatedValue(for:"Z"), "Upper")
-    XCTAssertNil(data.associatedValue(for:"["))
-    XCTAssertNil(data.associatedValue(for:"`"))
-    XCTAssertEqual(data.associatedValue(for:"a"), "Lower")
-    XCTAssertEqual(data.associatedValue(for:"m"), "Lower")
-    XCTAssertEqual(data.associatedValue(for:"z"), "Lower")
-    XCTAssertNil(data.associatedValue(for:"{"))
-    XCTAssertNil(data.associatedValue(for:"あ"))
+    let data = _UnicodeAssociativeArray<String>(array)
+    let check = { (key:Unicode.Scalar, expected:String?) -> Void in
+      XCTAssertEqual(data.value(for:key), expected)
+    }
+    check("0", nil)
+    check("@", nil)
+    check("A", "Upper")
+    check("M", "Upper")
+    check("Z", "Upper")
+    check("[", nil)
+    check("`", nil)
+    check("a", "Lower")
+    check("m", "Lower")
+    check("z", "Lower")
+    check("{", nil)
+    check("あ", nil)
   }
   
   func test_UnicodePredicate() {
     let array: [UInt32] = [0x20819, 0x30819] // "A"..."Z", "a"..."z"
-    let predicate = _UnicodePredicate(array, sort: false)
-    XCTAssertFalse(predicate.contains("0"))
-    XCTAssertFalse(predicate.contains("@"))
-    XCTAssertTrue(predicate.contains("A"))
-    XCTAssertTrue(predicate.contains("M"))
-    XCTAssertTrue(predicate.contains("Z"))
-    XCTAssertFalse(predicate.contains("["))
-    XCTAssertFalse(predicate.contains("`"))
-    XCTAssertTrue(predicate.contains("a"))
-    XCTAssertTrue(predicate.contains("m"))
-    XCTAssertTrue(predicate.contains("z"))
-    XCTAssertFalse(predicate.contains("{"))
-    XCTAssertFalse(predicate.contains("あ"))
+    let predicate = _UnicodePredicate(array)
+    let check = { (key:Unicode.Scalar, expected:Bool) -> Void in
+      if expected { XCTAssertTrue(predicate.contains(key)) }
+      else { XCTAssertFalse(predicate.contains(key)) }
+    }
+    check("0", false)
+    check("@", false)
+    check("A", true)
+    check("M", true)
+    check("Z", true)
+    check("[", false)
+    check("`", false)
+    check("a", true)
+    check("m", true)
+    check("z", true)
+    check("{", false)
+    check("あ", false)
   }
   
   
