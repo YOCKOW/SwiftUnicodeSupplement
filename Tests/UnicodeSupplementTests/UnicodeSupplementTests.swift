1635dc25c3dac46af8838f9f011cef4ee941e07e
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-14 14:46:21 +0900

    Add tests for IDNAStatus

--- Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
+++ Tests/UnicodeSupplementTests/UnicodeSupplementTests.swift
@@ -2,13 +2,41 @@ import XCTest
 @testable import UnicodeSupplement
 
 final class UnicodeSupplementTests: XCTestCase {
-  func test() {
-    // How to test...
+  func test_IDNAStatus() {
+    let check = { (scalar:Unicode.Scalar, std3:Bool, idna2008:Bool,
+                   expected:Unicode.IDNAStatus?) -> Void in
+      XCTAssertEqual(scalar.latestProperties.idnaStatus(usingSTD3ASCIIRules:std3,
+                                                        idna2008Compatible:idna2008),
+                     expected)
+    }
+    
+    // not exhaustive
+    check("\u{A1}", true, false, .valid)
+    check("\u{A1}", true, true, .disallowed)
+    check("\u{19DA}", true, false, .valid)
+    check("\u{19DA}", true, true, .disallowed)
+    
+    check("あ", true, false, .valid)
+    
+    check("\u{AD}", true, false, .ignored)
+    
+    check("A", true, false, .mapped(["a"]))
+    
+    check("\u{DF}", true, false, .deviation(["s", "s"]))
+    check("\u{200C}", true, false, .deviation([]))
+    
+    check("\u{04C0}", true, false, .disallowed)
+    
+    check("?", true, false, .valid)
+    check("?", false, false, .disallowed)
+    
+    check("\u{2474}", true, false, .mapped(["(", "1", ")"]))
+    check("\u{2474}", false, false, .disallowed)
   }
   
   
   static var allTests: [(String, (UnicodeSupplementTests) -> () -> ())] = [
-    ("test", test),
+    ("test_IDNAStatus", test_IDNAStatus),
   ]
 }
 
