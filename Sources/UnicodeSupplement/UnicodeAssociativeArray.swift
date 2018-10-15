f19df4fb46eea3c2d2758bcb1728c01a8a31e38e
YOCKOW <YOCKOW@users.noreply.github.com>
2018-10-15 11:57:08 +0900

    Use cache in _UnicodeAssociativeArray

--- Sources/UnicodeSupplement/UnicodeAssociativeArray.swift
+++ Sources/UnicodeSupplement/UnicodeAssociativeArray.swift
@@ -5,7 +5,10 @@
      See "LICENSE.txt" for more information.
  ************************************************************************************************ */
 
-internal struct _UnicodeAssociativeArray<T> {
+internal final class _UnicodeAssociativeArray<T> {
+  private typealias _UnicodeScalarClosedRange = UInt32
+  private typealias _UnicodeScalarValue = UInt32
+  
   /// An array of (`UInt32`, `T`).
   ///
   /// Each integer indicates a range of Unicode scalar.
@@ -14,7 +17,15 @@ internal struct _UnicodeAssociativeArray<T> {
   ///
   /// The instance of `T` is associated with the Unicode scalars contained in the range.
   /// When `T` is `Void`, it represents just ranges of Unicode scalars.
-  private let _array: [(UInt32, T)]
+  private let _array: [(_UnicodeScalarClosedRange, T)]
+  
+  private enum _CachedValue {
+    /// value
+    case value(T)
+    /// nil
+    case none
+  }
+  private var _cacheTable: [_UnicodeScalarValue:_CachedValue] = [:]
   
   internal init(_ table:[(UInt32, T)], alreadySorted:Bool = false) {
     self._array = alreadySorted ? table : table.sorted { $0.0 < $1.0 }
@@ -22,14 +33,30 @@ internal struct _UnicodeAssociativeArray<T> {
   
   internal func value(for scalar:Unicode.Scalar) -> T? {
     let value = scalar.value
-    for pair in self._array {
-      let start = pair.0 >> 11
-      if value < start { return nil } // because _array must be sorted.
-      
-      let length = pair.0 & 0b11111111111
-      if value <= start + length { return pair.1 }
+    
+    if let cached = self._cacheTable[value] {
+      switch cached {
+      case .value(let actualValue): return actualValue
+      case .none: return nil
+      }
+    }  else {
+      for pair in self._array {
+        let start = pair.0 >> 11
+        if value < start {
+          // because _array must be sorted.
+          self._cacheTable[value] = .none
+          return nil
+        }
+        
+        let length = pair.0 & 0b11111111111
+        if value <= start + length {
+          self._cacheTable[value] = .value(pair.1)
+          return pair.1
+        }
+      }
+      self._cacheTable[value] = .none
+      return nil
     }
-    return nil
   }
   
   internal subscript(_ scalar:Unicode.Scalar) -> T? {
@@ -40,7 +67,7 @@ internal struct _UnicodeAssociativeArray<T> {
 internal typealias _UnicodePredicate = _UnicodeAssociativeArray<Void>
 
 extension _UnicodeAssociativeArray where T == Void {
-  internal init(_ ranges:[UInt32], alreadySorted:Bool = false) {
+  internal convenience init(_ ranges:[UInt32], alreadySorted:Bool = false) {
     assert(MemoryLayout<UInt32>.size == MemoryLayout<(UInt32, Void)>.size,
            "This method expects that the sizes of UInt32 and (UInt32, Void) are equal.")
     self.init(unsafeBitCast(ranges, to:Array<(UInt32, Void)>.self), alreadySorted:alreadySorted)
