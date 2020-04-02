c625bf6d1a9928df9d2fbc5e7867de5bf5bbb1be
YOCKOW <YOCKOW@users.noreply.github.com>
2020-04-02 18:00:33 +0900

    Update dependencies of updater.

--- utils/SwiftUnicodeSupplementUpdater/Package.swift
+++ utils/SwiftUnicodeSupplementUpdater/Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     .package(url: "https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.2"),
     .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.0"),
     .package(url: "https://github.com/YOCKOW/SwiftStringComposition.git", from: "1.1.0"),
-    .package(url: "https://github.com/YOCKOW/SwiftUnicodeSupplement.git", "0.7.1"..<"2.0.0"),
-    .package(url: "https://github.com/YOCKOW/ySwiftCodeUpdater.git", "1.2.0"..<"2.0.0"),
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", "0.9.0"..<"2.0.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "1.0.0"),
+    .package(url: "https://github.com/YOCKOW/ySwiftCodeUpdater.git", from: "1.2.1"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", "0.9.1"..<"2.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
